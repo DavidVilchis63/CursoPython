1ec00cd24447f295ea88cf6580b401f056ac935c
David Vilchis <davidvilchis63@gmail.com>
2024-10-04 16:16:46 -0600

    Primer leccion

---
+++ Dia 01/Leccion01.py
@@ -0,0 +1,11 @@
+# Primer ejercicio, mostrar en consola
+
+print("Hola Mundo");
+print(555);
+
+# Strings, cadenas de texto
+    # \n Salto de linea (nueva linea)
+    # \t Tabuladaor
+
+print("Hola" + " " + "Mundo")
+print("Hola" + " " + "Mundo\n Hola Mundo")
