bd57b31f8a7092f0bdc4887fad7a55813a1d9dca
David Vilchis <davidvilchis63@gmail.com>
2024-10-08 15:13:10 -0600

    Conversionesde tipo de datos

--- Dia 02/Leccion02.py
+++ Dia 02/Leccion02.py
@@ -19,3 +19,19 @@ print(type(num02))
 
 edad01 =  input("Coloca tu edad: ")
 print(type(edad01))
+
+
+# Conversiones
+
+num03 = 5.8
+print(num03)
+print(type(num03))
+
+num04 = int(num03)
+print(num04)
+print(type(num04))
+
+edad02 =  input("Coloca tu edad: ")
+print(type(edad02)) # De string a int para poder usar dato
+edad02 = int(edad02)
+print(type(edad02))
