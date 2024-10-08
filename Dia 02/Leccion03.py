0f298f02439e037d67fff144dc8b50ae670b4207
David Vilchis <davidvilchis63@gmail.com>
2024-10-08 16:24:42 -0600

    Operadores matematicos

---
+++ Dia 02/Leccion03.py
@@ -0,0 +1,27 @@
+# Operadores matematicos
+
+x = 6
+y = 6
+z = 8
+
+print(f"{x} mas {y} es igual a {x+y}")
+print(f"{x} menos {y} es igual a {x-y}")
+print(f"{x} por {y} es igual a {x*y}")
+print(f"{x} entre {y} es igual a {x/y}")
+
+    # Division al piso (Sin residuos)
+
+print(f"{z} dividio 'al piso' de {y} es igual a {z//y}")
+
+    # Modulo de la division (Residuo)
+
+print(f"{z} modulo de {y} es igual a {z%y}")
+
+    # Potencias
+
+print(f"{x} elevado a {y} es igual a {x**y}")
+print(f"{x} elevado a {2} es igual a {x**2}")
+
+    # Raiz cuadrada
+
+print(f"La raiz cuadrada de {x} es {x**0.5}")
