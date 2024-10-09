9c7a5021b6df2759c8b5ee0cfe53ae5f44800c46
David Vilchis <davidvilchis63@gmail.com>
2024-10-09 15:34:43 -0600

    Metodos para cadenas de texto

--- Dia 03/Leccion04.py
+++ Dia 03/Leccion04.py
@@ -17,3 +17,26 @@ texto2 = "ABCDEFGHIJKLMNO"
 fragmento = texto2[::-1] #El primerparametro, es el inicio, segundo es hasta donde lo va a extraer, y el ultimo sera si necesitas que se extraiga de 2 en 2 o culquier otra combinación
 print(fragmento)
 
+# Metodos para textos
+
+nuevoTexto = "Lorem ipsum dolor sit amet consectetur adipiscing elit, duis justo auctor facilisis ultrices molestie, lobortis lectus quis tincidunt sollicitudin nisi."
+
+resultado4 = nuevoTexto.upper()
+resultado5 = nuevoTexto.lower()
+resultado6 = nuevoTexto.split("a") #Creae lista cada espacio o si pones el caracter deseado, creara lista cada que aparesca ese caracter
+print(resultado4)
+print(resultado5)
+print(resultado6)
+
+a = "A"
+b = "B"
+c = "C"
+d = "E"
+e = " ".join([a,b,c,d]) #Junta las variables
+print(e)
+
+resultado7 = nuevoTexto.find("x") #Sirve para buscar caracteres, si no esta, el resultado sera un -1
+resultado8 = nuevoTexto.replace("e","x")
+
+print(resultado7)
+print(resultado8)
