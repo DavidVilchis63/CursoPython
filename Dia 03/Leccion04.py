659ee1a19561f50721573573961ea7d9f43fd776
David Vilchis <davidvilchis63@gmail.com>
2024-10-09 15:07:49 -0600

    Funcion index

---
+++ Dia 03/Leccion04.py
@@ -0,0 +1,11 @@
+# Funcion index
+
+texto = "Este es una prueba"
+resultado = texto[0]
+print(resultado)
+
+resultado2 = texto.index("s")
+print(resultado2)
+
+resultado3 = texto.rindex("a")
+print(resultado3)
