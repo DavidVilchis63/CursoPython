266147708143b29c29c6bfc4252279009700ba20
David Vilchis <davidvilchis63@gmail.com>
2024-10-09 15:13:51 -0600

    Extraer texto

--- Dia 03/Leccion04.py
+++ Dia 03/Leccion04.py
@@ -4,8 +4,15 @@ texto = "Este es una prueba"
 resultado = texto[0]
 print(resultado)
 
-resultado2 = texto.index("s")
+resultado2 = texto.index("s") #Busca de izquierda a derecha
 print(resultado2)
 
-resultado3 = texto.rindex("a")
+resultado3 = texto.rindex("a") #Busca de derecha a izquierda
 print(resultado3)
+
+
+# Extraer cadenas de texto
+
+texto2 = "ABCDEFGHIJKLMNO"
+fragmento = texto2[::-1] 
+print(fragmento)
