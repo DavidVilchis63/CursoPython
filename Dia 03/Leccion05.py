8201b3f4c1ee54a444adf4624399a4b391f46c50
David Vilchis <davidvilchis63@gmail.com>
2024-10-09 16:18:28 -0600

    Metodos para cadenas de texto

---
+++ Dia 03/Leccion05.py
@@ -0,0 +1,9 @@
+# Propiedades de strings
+
+poema = """Mil pequeños peces blancos
+como si hirviera
+el color del agua"""
+
+print("agua" in poema) #Esta en texto
+print("agua" not in poema) # No esta entexto
+print(len(poema)) #Numero de caracteres
