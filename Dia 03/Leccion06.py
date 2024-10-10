e32357637ebd6ba1fd35f89bd458ddfdece6a0b7
David Vilchis <davidvilchis63@gmail.com>
2024-10-10 14:51:45 -0600

    Sets

--- Dia 03/Leccion06.py
+++ Dia 03/Leccion06.py
@@ -8,3 +8,24 @@ print(len(tupla))
 print(tupla.count(1)) #Cuenta las veces que aparece el valor en la tupla
 
 print(tupla.index(1))
+
+# Sets
+
+mi_set = set([1,2,3,4,5,6,7])
+
+print(type(mi_set))
+print(len(mi_set))
+print(4 in mi_set)
+
+mi_set2 = {2,6,7,1,9,10}
+mi_set3 = mi_set.union(mi_set2)
+print(mi_set3)
+
+mi_set.add(8)
+print(mi_set)
+
+mi_set.remove(8)
+print(mi_set)
+
+mi_set.clear()
+print(mi_set)
