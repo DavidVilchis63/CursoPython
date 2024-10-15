4935b159da048ff303cd0a01b3335fcd5e9a65f9
David Vilchis <davidvilchis63@gmail.com>
2024-10-15 16:05:21 -0600

    Operadores logicos

--- Dia 04/Leccion07.py
+++ Dia 04/Leccion07.py
@@ -1,4 +1,8 @@
 # Operadores de comparación
+    # Iguak a ==
+    # Diferente de !=
+    # Mayor y mayor iguak > >=
+    # Menor y menor igual < <=
 
 mi_bool = 10==10
 mi_bool01 = "100" == 100
@@ -6,3 +10,15 @@ mi_bool01 = "100" == 100
 
 print(mi_bool)
 print(mi_bool01)
+
+
+# Operadores logicos
+
+mi_bool02 = (4 < 5) and (5 == 2+3)
+print(mi_bool02)
+
+mi_bool03 = (4 < 5) or (5 == 2+3)
+print(mi_bool03)
+
+mi_bool04 = not (5 == 2+3)
+print(mi_bool04)
