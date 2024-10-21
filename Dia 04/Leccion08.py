81390debd9f04dae3202160daa9be08549d915b6
David Vilchis <davidvilchis63@gmail.com>
2024-10-21 15:00:31 -0600

    Funcion IF

---
+++ Dia 04/Leccion08.py
@@ -0,0 +1,12 @@
+# Función if
+
+letra = "b"
+
+if letra == "a":
+    print("Se cumplio el if y entro a codigo")
+
+elif letra == "b":
+    print("Entra a esta parte del codigo")
+
+else:
+    print("No entro a codigo")
