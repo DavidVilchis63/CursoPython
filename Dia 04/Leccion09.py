60b386fcf995845dd4853fa5a1ad38203e49df2d
David Vilchis <davidvilchis63@gmail.com>
2025-02-11 17:07:19 -0600

    Funcion Zip

---
+++ Dia 04/Leccion09.py
@@ -0,0 +1,13 @@
+# Funcion Zip
+
+nombres = ["Ana", "Hugo", "Valeria"];
+edades = [69,29,42];
+ciudades = ["Lima", "Madrid", "Mexico"]
+
+combinados = list(zip(nombres, edades,ciudades));
+
+print(combinados)
+
+for nombre, edad, ciudad in combinados:
+    print(f"{nombre} tiene {edad} años y vive en {ciudad}")
+    
