6b92d1b46a8b8b1c6116e6ee7d42efb6bb7e81d7
David Vilchis <davidvilchis63@gmail.com>
2025-02-11 19:00:45 -0600

    Funcion match

--- Dia 04/Leccion10.py
+++ Dia 04/Leccion10.py
@@ -31,3 +31,18 @@ lista = [letra for letra in palabra];
 lista2 = [n for n in range(0,21,2)];
 print(lista);
 print(lista2);
+
+# Funcion match
+
+serie = "N-02";
+
+match serie:
+    case "N-01":
+        print("Samsung")
+    case "N-02":
+        print("Nokia")
+    case "N-03":
+        print("Motorola")
+    case _:
+        print("No existe")
+
