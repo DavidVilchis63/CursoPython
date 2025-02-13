53494699bdf6cc4aee6834020d873ad279ce735a
David Vilchis <davidvilchis63@gmail.com>
2025-02-12 19:39:49 -0600

    Como utilizar return,

--- Dia 05/Leccion11.py
+++ Dia 05/Leccion11.py
@@ -7,7 +7,7 @@ saludar("David") # De esta forma se llama a la función
 
 
 def multiplicar(numero1, numero2):
-    return numero1 * numero2
+    return numero1 * numero2 # Se utiliza return para devolver un valor
 
 resultado = multiplicar(5, 3)
 print(resultado) # De esta forma se imprime el resultado de la función
