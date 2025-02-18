d45e41c607e4b0b6b446751feadc568e0dbd2c1f
David Vilchis <davidvilchis63@gmail.com>
2025-02-17 21:12:41 -0600

    Ejemplos de funciones

--- Dia 05/Leccion11.py
+++ Dia 05/Leccion11.py
@@ -11,3 +11,24 @@ def multiplicar(numero1, numero2):
 
 resultado = multiplicar(5, 3)
 print(resultado) # De esta forma se imprime el resultado de la función
+
+
+def check_3_cifras(numero):
+    return numero in range(100, 1000)
+
+resultado01 = check_3_cifras(5000);
+print(resultado01)
+
+def check_3_cifras_en_lista(lista):
+
+    lista_numeros = [];
+
+    for numero in lista:
+        if numero in range(100, 1000):
+            lista_numeros.append(numero);
+        else:
+            pass
+    return lista_numeros
+
+resultado02 = check_3_cifras_en_lista([700, 10, 910, 100])
+print(resultado02)
