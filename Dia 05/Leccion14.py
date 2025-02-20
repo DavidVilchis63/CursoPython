a6f30f8efc17c95f91c3385af2e9b968e55518a0
David Vilchis <davidvilchis63@gmail.com>
2025-02-19 21:33:11 -0600

    Argumentos indefinidos con clave

--- Dia 05/Leccion14.py
+++ Dia 05/Leccion14.py
@@ -7,3 +7,18 @@ def sumar(*args):
     return suma
 
 print(sumar(1,2,3,4,5,6,7,8,9,110))
+
+# Argumentos indefinidos con clave
+
+def sumar(**kwargs):
+
+    total = 0
+
+    for clave, valor in kwargs.items():
+        print(f"{clave} = {valor}")
+        total += valor
+
+    return total
+
+print(sumar(x=3, y=5, z=2))
+
