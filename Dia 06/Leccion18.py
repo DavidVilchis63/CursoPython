6b8c689fc9edfb9f5620c1cb00cfd91f54f75255
DavidVilchis2 <davidvilchis63@gmail.com>
2025-04-24 02:57:40 -0600

    Modulo pathlib

--- Dia 06/Leccion18.py
+++ Dia 06/Leccion18.py
@@ -1,1 +1,8 @@
-#Prueba de cambio 
+#Modulo Pathlib
+
+from pathlib import Path
+
+carpeta = Path(r"C:\Users\david\Documents\Python\Dia 06")
+
+print(carpeta.read_text())      #Para este no se necesita abrir y cerrar archivo
+
