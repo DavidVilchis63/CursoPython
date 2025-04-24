d313e12cb1205822ab11d7e5e0fabcc07485f9eb
David Vilchis <davidvilchis63@gmail.com>
2025-04-24 17:05:26 -0600

    Correcion path

--- Dia 06/Leccion18.py
+++ Dia 06/Leccion18.py
@@ -2,7 +2,7 @@
 
 from pathlib import Path
 
-carpeta = Path(r"C:\Users\david\Documents\Python\Dia 06")
+carpeta = Path("C:/Users/david/Documents/Python/Dia 06/Prueba00.txt") 
 
 print(carpeta.read_text())      #Para este no se necesita abrir y cerrar archivo
 
