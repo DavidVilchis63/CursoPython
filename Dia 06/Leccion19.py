775f260a172cfecb981b2074c37c336205e19607
David Vilchis <davidvilchis63@gmail.com>
2025-04-24 17:29:08 -0600

    Modulo pathlib 2

---
+++ Dia 06/Leccion19.py
@@ -0,0 +1,12 @@
+#Modulo pathlib 2
+
+from pathlib import Path
+
+base = Path.home()                                  #Obtiene la ruta del directorio de inicio del usuario
+guia = Path(base, "España", "Barcelona", "Sagrada Familia.txt")   #Crea un objeto de tipo Path con la ruta especificada
+print(base)
+print(guia) 
+
+guia02 = guia.with_name("Guia02.txt")                             #Cambia el nombre del archivo
+print(guia02)
+
