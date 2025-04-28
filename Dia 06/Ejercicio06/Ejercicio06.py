0674b87e28b17df6bb73194f7409f47c75df7a5a
David Vilchis <davidvilchis63@gmail.com>
2025-04-28 15:54:07 -0600

    Bienvenida y ruta

--- Dia 06/Ejercicio06/Ejercicio06.py
+++ Dia 06/Ejercicio06/Ejercicio06.py
@@ -1,2 +1,16 @@
 #Creacion de un almacen de recetas
 
+#Modulos usados
+from pathlib import Path
+
+#Bienvenida al programa
+
+ruta = Path(Path.home(), "Documents", "Python", "Dia 06", "Ejercicio06", "Recetas")
+print(ruta)
+
+nombre = input("¿Cuál es tu nombre? ")
+print(f"Bienvenido al Admin de Recetas {nombre}")
+print("La ruta de la carpeta de recetas es: ", ruta)
+
+
+
