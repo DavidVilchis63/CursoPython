e20ed62e82e4306b89bf29617d7728cf7cc45375
David Vilchis <davidvilchis63@gmail.com>
2025-05-06 15:03:49 -0600

    Clases, POO

---
+++ Dia 07/Leccion20.py
@@ -0,0 +1,15 @@
+#Programacion orientada a objetos
+
+#Clases
+class Pajaro:
+    pass
+
+
+#Instancias
+miPajaro = Pajaro()
+otroPajaro = Pajaro()
+
+
+print(miPajaro)
+print(otroPajaro)
+print(type(miPajaro))
