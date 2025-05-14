cbfd74ff1537e55ff06d2bd7f642d6dd5c1b69ea
David Vilchis <davidvilchis63@gmail.com>
2025-05-14 08:32:04 -0600

    Ejercicio dia 7, metodo retirar

--- Dia 07/Ejercicio07.py
+++ Dia 07/Ejercicio07.py
@@ -19,18 +19,24 @@ class Cliente(Persona):
         return f"Hola {self.nombre + " " +self.apellido} tu No. de cuenta es: {self.cuenta} con un saldo de: {self.balance}"
     
     # Metodo depositar
-
-    def depositar(self):
-        
+    def depositar(self):        
         saldoAgregar = int(input("Cantidad a depositar: "))
         self.balance = self.balance + saldoAgregar
         print(f"Tu nuevo saldo es: {self.balance}")
 
     # Metodo retirar
-    
+    def retirar(self):
+        saldoRetirar = int(input("Cantidad que desea retirar: "))
+
+        if saldoRetirar <= self.balance:
+            self.balance = self.balance - saldoRetirar
+            print(f"Retiro exitoso, su nuevo saldo es {self.balance}")
+        else:
+            print(f"No es posible retirar ya uqe ingreso una cantidad mayor a su saldo, su saldo es: {self.balance}")
 
 
 cliente01 = Cliente("David", "Vilchis", 5563187248, 20000)
 
 print(cliente01)
 cliente01.depositar()
+cliente01.retirar()
