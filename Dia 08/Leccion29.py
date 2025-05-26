32282738f06a1106b53db484043935cf9aace1b2
David Vilchis <davidvilchis63@gmail.com>
2025-05-26 14:51:06 -0600

    Uso de pylint

---
+++ Dia 08/Leccion29.py
@@ -0,0 +1,15 @@
+"""
+Formato de conveccion
+Modulo pylint
+
+Codigo para verificar desde el CMD
+python -m pylint Leccion29.py -r y
+"""
+
+
+def unaFuncion():
+    numero = 1
+    print(numero)
+
+
+unaFuncion()
