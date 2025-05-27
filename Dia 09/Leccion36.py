16acfdf85ac71616a1ff9ab15fea95788173758f
David Vilchis <davidvilchis63@gmail.com>
2025-05-27 13:21:07 -0600

    Modulo math

---
+++ Dia 09/Leccion36.py
@@ -0,0 +1,28 @@
+"""
+    Modulo Math
+"""
+
+import math
+
+resultado = math.floor(89.665)
+print(resultado)
+
+
+resultado = math.ceil(89.665)
+print(resultado)
+
+
+resultado = math.pi
+print(resultado)
+
+
+resultado = math.log(25, 5)
+print(resultado)
+
+
+resultado = math.tan(2050)
+print(resultado)
+
+
+resultado = math.cos(2050)
+print(resultado)
