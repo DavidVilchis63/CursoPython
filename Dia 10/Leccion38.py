c6ef42be419802da4032585c641e56bd7f42f30a
David Vilchis <davidvilchis63@gmail.com>
2025-05-28 14:53:52 -0600

    Movimiento a jugador

--- Dia 10/Leccion38.py
+++ Dia 10/Leccion38.py
@@ -19,9 +19,10 @@ pygame.display.set_icon(icono)
 imgJugador = pygame.image.load("cohete.png")
 jugadorX = 368
 JugadorY = 536
+jugadorXCambio = 0
 
-def jugador():
-    pantalla.blit(imgJugador, (jugadorX, JugadorY))
+def jugador(x, y):
+    pantalla.blit(imgJugador, (x, y))
 
 #Loop del juego
 seEjecuta = True
@@ -35,8 +36,20 @@ while seEjecuta:
         if evento.type == pygame.QUIT:
             seEjecuta = False
 
+        if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_LEFT:
+                jugadorXCambio = -0.3
+            if evento.key == pygame.K_RIGHT:
+                jugadorXCambio = 0.3
+        
+        if evento.type == pygame.KEYUP:
+            if evento.key == pygame.K_LEFT or evento.key == pygame.K_RIGHT:
+                jugadorXCambio = 0
+
+
     #Jugador
-    jugador()
+    jugadorX += jugadorXCambio
+    jugador(jugadorX, JugadorY)
 
     #Actualiza pantalla
     pygame.display.update()
