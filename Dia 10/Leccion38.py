a98649f24c0d035597499bfeabbcdad2fbf1901c
David Vilchis <davidvilchis63@gmail.com>
2025-05-28 14:25:55 -0600

    Se agrega jugador

--- Dia 10/Leccion38.py
+++ Dia 10/Leccion38.py
@@ -15,14 +15,28 @@ pygame.display.set_caption("Invasion Espacial")
 icono = pygame.image.load("ovni.png")
 pygame.display.set_icon(icono)
 
+#Crear jugador
+imgJugador = pygame.image.load("cohete.png")
+jugadorX = 368
+JugadorY = 536
+
+def jugador():
+    pantalla.blit(imgJugador, (jugadorX, JugadorY))
+
 #Loop del juego
 seEjecuta = True
 
 while seEjecuta:
+
+    #Cambiar color de pantalla RGB
+    pantalla.fill((205,144,228))
+
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             seEjecuta = False
 
-    #Cambiar color de pantalla
-    pantalla.fill((205,144,228))
+    #Jugador
+    jugador()
+
+    #Actualiza pantalla
     pygame.display.update()
