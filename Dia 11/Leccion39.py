0e87348c7cf234a3da8d874bde64b0148bdaaaf8
David Vilchis <davidvilchis63@gmail.com>
2025-06-02 22:54:59 -0600

    Bs4 y requests

--- Dia 11/Leccion39.py
+++ Dia 11/Leccion39.py
@@ -4,3 +4,18 @@
 
 
 import bs4
+import requests
+
+#Buscar variable con requests
+
+resultado = requests.get("https://escueladirecta-blog.blogspot.com/2021/10/encapsulamiento-pilares-de-la.html")
+print(type(resultado))
+#print(resultado.text)
+
+#Metodos de BS4
+
+sopa = bs4.BeautifulSoup(resultado.text, "lxml")
+#print(sopa)
+print(sopa.select("title"))
+print(len(sopa.select("p")))
+print(sopa.select("title")[0].getText())
