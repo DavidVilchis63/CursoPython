6785d96273eec3d7487f9b973c03efbbc8e35262
David Vilchis <davidvilchis63@gmail.com>
2025-06-03 00:22:30 -0600

    Ejercicio parte 1, buscar elementos para realizar busqueda

--- Dia 11/Leccion41.py
+++ Dia 11/Leccion41.py
@@ -12,3 +12,9 @@ sopa = bs4.BeautifulSoup(resultado.text, "lxml")
 
 print(len(sopa.select(".product_pod")))
 
+libros = sopa.select(".product_pod")
+ejemplo = libros[0].select(".star-rating.Four")
+print(ejemplo)
+
+ejemplo = libros[0].select("a")[1]["title"]
+print(ejemplo)
