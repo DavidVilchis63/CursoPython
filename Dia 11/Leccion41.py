a4c4965dcf1db37643034df6869e51fa2b7f986d
David Vilchis <davidvilchis63@gmail.com>
2025-06-03 00:14:19 -0600

    Ejercicio parte 1, ver cantidad de elementos de una etiqueta

--- Dia 11/Leccion41.py
+++ Dia 11/Leccion41.py
@@ -7,5 +7,8 @@ import requests
 
 urlBase = "https://books.toscrape.com/catalogue/page-{}.html"
 
-for n in range(1,11):
-    print(urlBase.format(n))
+resultado = requests.get(urlBase.format("1"))
+sopa = bs4.BeautifulSoup(resultado.text, "lxml")
+
+print(len(sopa.select(".product_pod")))
+
